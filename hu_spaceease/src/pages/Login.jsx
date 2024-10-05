d8b41370f474f50f9a908cfca499d87ff338f2fd
Jibran023 <js08312@st.habib.edu.pk>
2024-10-05 20:35:41 +0500

    Login page update
    login page

--- hu_spaceease/src/pages/Login.jsx
+++ hu_spaceease/src/pages/Login.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
-import './Login.css'
+import './Login.css';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  const navigate = useNavigate(); // Get the navigation function
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
-    e.preventDefault(); // Prevent page reload on form submit
+    e.preventDefault();
     // Add your login logic here (e.g., API call to authenticate)
 
     // On successful login, navigate to the dashboard
     navigate('/dashboard');
   };
 
-    return (
-      <>
-        <div className='navbar2'>
-          <div className='logo-container'>
-            <img src="images/hu_loho.PNG" alt="Habib University Logo" className='navbar-logo'/>
-          </div>
+  return (
+    <>
+      <div className='navbar2'>
+        <div className='logo-container'>
+          <img src="images/hu_loho.PNG" alt="Habib University Logo" className='navbar-logo'/>
         </div>
-        <div className='app-container'>
-          <div className='image-container'>
-            <img src="images/audi.jpg" alt="Login Image" className="login-image" />
-          </div>
+      </div>
+
+      <div className="login-wrapper">
+        <div className="semi-circle">
           <div className='login-container'>
-          <h1 className='main-heading'>HU-SpaceEase</h1>
+            <h1 className='main-heading'>HU-SpaceEase</h1>
             <div className='form-background'>
               <h2 className='login-heading'>Welcome!</h2>
               <form onSubmit={handleLogin}>
@@ -42,8 +41,9 @@ function Login() {
             </div>
           </div>
         </div>
-      </>
-    )
-  }
-  
-  export default Login
+      </div>
+    </>
+  );
+}
+
+export default Login;
