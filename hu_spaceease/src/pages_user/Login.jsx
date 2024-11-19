5625d864bb72a58698fb9945c909d883764537d4
Alizain7866 <am07565@st.habib.edu.pk>
2024-11-19 11:21:53 +0500

    sturctured_it

--- hu_spaceease/src/pages_user/Login.jsx
+++ hu_spaceease/src/pages_user/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('http://localhost:5000/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
