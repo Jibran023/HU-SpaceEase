4426113c2d096e1b6b5eb422cb16bccf675136cc
Alizain7866 <am07565@st.habib.edu.pk>
2024-11-20 17:14:24 +0500

    hashing-done

--- hu_spaceease/models/booking.js
+++ hu_spaceease/models/booking.js
@@ -11,19 +11,17 @@
 
 //////////////////////////////////////////
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // Define Booking schema
 const BookingSchema = new mongoose.Schema({
-    studentId: { type: String, required: true },
-    roomId: { type: String, required: true },
-    bookingTime: { type: Date, required: true },
-    reason: { type: String, required: true },
-    status: { type: String, default: 'Pending' }, // You can have other statuses like 'Approved', 'Rejected'
+  studentId: { type: String, required: true },
+  roomId: { type: String, required: true },
+  bookingTime: { type: Date, required: true },
+  reason: { type: String, required: true },
+  status: { type: String, default: "Pending" }, // You can have other statuses like 'Approved', 'Rejected'
 });
 
 // Export the Booking model as the default export
-export default mongoose.model('Booking', BookingSchema);
+export default mongoose.model("Booking", BookingSchema);
 /////////////////////////////////////////////////////////////
-
-
